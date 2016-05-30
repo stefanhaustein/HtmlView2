09972b733041d5cdb384721d3f15a61e20f3079e
Stefan Haustein <stefan.haustein@gmail.com>
2016-05-31 00:19:23 +0200

    HtmlContext to PageContext refactoring

--- src/main/java/org/kobjects/htmllayout/demo/MainActivity.java
+++ src/main/java/org/kobjects/htmllayout/demo/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.ScrollView;
 
 import org.kobjects.htmllayout.DefaultRequestHandler;
-import org.kobjects.htmllayout.HtmlContext;
+import org.kobjects.htmllayout.PageContext;
 import org.kobjects.htmllayout.HtmlLayout;
 import org.kobjects.htmllayout.parser.HtmlProcessor;
 
@@ -13,8 +13,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
+
+  HtmlProcessor htmlProcessor = new HtmlProcessor();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +28,16 @@ public class MainActivity extends AppCompatActivity {
       Reader reader = new BufferedReader(
           new InputStreamReader(getAssets().open("index.html")));
 
-      HtmlContext htmlContext = new HtmlContext(this, new DefaultRequestHandler(this));
-      HtmlProcessor parser = new HtmlProcessor(htmlContext);
-      HtmlLayout html = parser.parse(reader);
+      PageContext pageContext = new PageContext(this, new DefaultRequestHandler(this),
+          new URI("file:///android_asset/"));
+      HtmlLayout html = htmlProcessor.parse(reader, pageContext);
 
       ScrollView scrollView = new ScrollView(this);
       scrollView.addView(html);
       setContentView(scrollView);
 
-    } catch (IOException e) {
+    } catch (Exception e) {
       throw new RuntimeException(e);
     }
-
-
   }
 }
